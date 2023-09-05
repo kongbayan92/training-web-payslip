a8396f90033f193e8611619185bbd4c6f9bfcddd
Yanzen Project <yanwarsolah@gmail.com>
2023-09-05 19:17:39 +0700

    D12 - 2

---
+++ src/pages/PageUsers.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const PageUsers = () => {
+  <Container>
+    <Row>
+      <Col>
+        <h3>Users</h3>
+      </Col>
+    </Row>
+  </Container>
+}
+
+export default PageUsers;
