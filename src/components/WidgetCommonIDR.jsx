9066d72b1fa684947cdf69051829f6b588a91d81
Yanzen Project <yanwarsolah@gmail.com>
2023-09-11 20:37:59 +0700

    D16 - 03.01

--- src/components/WidgetCommonIDR.jsx
+++ src/components/WidgetCommonIDR.jsx
@@ -1,5 +1,5 @@
 const WidgetCommonIDR = ({value}) => {
-  return <>{value.toLocaleString('id-ID', {style: 'currency', currency: 'IDR'})}</>
+  return <>{value ? value.toLocaleString('id-ID', {style: 'currency', currency: 'IDR'}) : 0}</>
 }
 
 export default WidgetCommonIDR;
